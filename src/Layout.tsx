13522e9c049ff6429fee985bcb7137ad8e1101b4
chaosleader <134308461+chaosleader@users.noreply.github.com>
2024-02-06 20:04:23 -0500

    made site worky on mobile

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,29 +1,29 @@
 import React from "react";
 import "./Layout.css";
-import SLogo from "./images/SuperNova
+import SLogo from "./images/SupernovaSLogoTransparent.png"
 import  PlanetLogo from "./images/SupernovaPlanetLogoTransparent.png";
 import {Link, Outlet} from "react-router-dom";
 const Layout = () => {
     return (
         <div id = "page">
             <div className = "layout">
-                <div className= "logo">
-                    <img className= "s-logo" src= {SLogo} alt = "A logo for supernova"/>
-                </div>
+                {/*<div className= "logo">*/}
+                {/*    <img className= "s-logo" src= {SLogo} alt = "A logo for supernova"/>*/}
+                {/*</div>*/}
             <nav>
                 <ul>
-                    <li>
+                    <li className= "li-0">
                         <Link className= "link" to="/">Home</Link>
                     </li>
-                    <li>
+                    <li className = "li-1">
                         <Link className= "link" to="/meet">Meet the team</Link>
                     </li>
-                    <li><Link className = "link" to = "/outreach">Outreach</Link></li>
+                    <li className= "li-2"><Link className = "link" to = "/outreach">Outreach</Link></li>
                 </ul>
             </nav>
-                <div className="logo">
-                    <img className= "planetLogo" src={PlanetLogo} alt="a logo for supernova featuring a plantet with Supernova.exe written on it"/>
-                </div>
+                {/*<div className="logo">*/}
+                {/*    <img className= "planetLogo" src={PlanetLogo} alt="a logo for supernova featuring a plantet with Supernova.exe written on it"/>*/}
+                {/*</div>*/}
             </div>
             <Outlet/>
         </div>
