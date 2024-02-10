4f227fea6e5eb2c8d63c79b21fed64d4d6c3b0ea
chaosleader <134308461+chaosleader@users.noreply.github.com>
2024-02-09 19:52:29 -0500

    updated mobile nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import './App.css';
 import {HashRouter, Routes, Route} from "react-router-dom";
 import HomePage from "./HomePage";
 import Layout from "./Layout";
 import MeetTheTeamPage from "./MeetTheTeamPage";
-import OutreachPage from "./OutreachPage";
+import OutreachPages from "./OutreachPages";
 import SponsorsPage from "./SponsorsPage";
 
 const App : React.FC = () => {
+    const [stickyFooter, setStickyFooter] = useState(false);
+    const content = useRef(null);
+
+    useEffect(() => {
+        const resizeObserver = new ResizeObserver(entries => {
+            for (let entry of entries) {
+                if (entry.target === content.current) {
+                    if (entry.contentRect.height < window.innerHeight - 91) {
+                        setStickyFooter(true);
+                    } else {
+                        setStickyFooter(false);
+                    }
+                }
+                console.log(entry.contentRect.height, window.innerHeight, stickyFooter);
+            }
+        });
+
+        if (content.current) {
+            resizeObserver.observe(content.current);
+        }
+
+        return () => {
+            if (content.current) {
+                resizeObserver.unobserve(content.current);
+            }
+        };
+    }, []);
     return (
         <div className="App">
+            <div ref = {content}>
             <HashRouter>
                 <Routes>
                     <Route path="*" element={<Layout/>}>
                         <Route index element={<HomePage/>}/>
                         <Route path="meet" element={<MeetTheTeamPage/>}/>
-                        <Route path="outreach" element={<OutreachPage/>}/>
+                        <Route path="outreach" element={<OutreachPages/>}/>
                         <Route path="sponsors" element={<SponsorsPage/>}/>
                     </Route>
                 </Routes>
             </HashRouter>
             <br/><br/><br/>
+        </div>
+            <footer style={stickyFooter? {position : "absolute", bottom : "0", width: "100vw"} : {marginTop : "auto"}}>
+                <h1>Supernova.exe 21774</h1>
+                <p>Email : <a style={{textDecoration: "none", color: "black"}}
+                              href="mailto:supernova21774@gmail.com">supernova21774@gmail.com</a></p>
+                <p>©2024 Supernova.exe 21774</p>
+                <br/>
+            </footer>
         </div>
     );
 }
