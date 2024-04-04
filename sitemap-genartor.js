3767f95633e517d3055361fe268c50ae7f34a78f
chaosleader <134308461+chaosleader@users.noreply.github.com>
2024-04-03 20:36:58 -0400

    stuff

--- sitemap-genartor.js
+++
@@ -1,9 +0,0 @@
-const { Sitemap } = require('react-router-sitemap');
-const { routes } = require('./src/routes'); // import your routes
-
-// Extract paths from routes
-const paths = routes.map(route => route.path);
-
-// Generate sitemap
-const sitemap = new Sitemap(routes).build('https://supernova-exe.github.io/', paths);
-sitemap.save('./public/sitemap.xml');
